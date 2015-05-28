0a4c3369b16410c0142da78a0062199a098404fb
willowcheng <e.wingus@Gmail.com>
2015-05-28 13:35:54 -0400

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Liu Cheng on 2015-05-28.
+//  Copyright (c) 2015 Liu Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
